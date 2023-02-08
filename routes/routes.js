536cf838c4c4a9d1203423575816e2a9dbd48ec2
Madhushanka <65624662+MadhushankaUCSC@users.noreply.github.com>
2023-02-09 00:00:39 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -18,6 +18,8 @@ router.route('/customer/create').post(customerController.customerRegister)
 router.route('/customer/update/:id').put(customerController.customerUpdate)
 router.route('/customer/remove/:id').delete(customerController.customerRemove)
 router.route('/customer/:id').get(customerController.customer)
+router.route('/customer/login').post(customerController.customerLogin)
+
 
 router.route('/order/all').get(orderController.orderGetAll)
 router.route('/order/create').post(orderController.orderCreate)
